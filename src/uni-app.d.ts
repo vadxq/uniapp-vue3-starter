d12e951d779daf966735d72d654ed2f8f326ab33
vadxq <dongxianlin@vadxq.com>
2021-09-18 14:10:15 +0800

    feat(init): inti project

---
+++ src/uni-app.d.ts
@@ -0,0 +1,6 @@
+import 'vue';
+declare module 'vue' {
+  type Hooks = App.AppInstance & Page.PageInstance;
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface ComponentCustomOptions extends Hooks {}
+}
